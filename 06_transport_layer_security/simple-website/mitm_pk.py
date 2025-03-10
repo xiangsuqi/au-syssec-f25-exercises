e9194001477d38ab263b4520ccea8dfb45fa0159
Diego F. Aranha <dfaranha@gmail.com>
2025-03-10 10:46:44 +0100

    Remove JSON option for key.

--- 06_transport_layer_security/simple-website/mitm_pk.py
+++ 06_transport_layer_security/simple-website/mitm_pk.py
@@ -12,12 +12,3 @@ def response(flow: http.HTTPFlow) -> None:
             'TODO: replace this string with the public key in PEM format',
             {'content-type': 'text/plain'},
         )
-    elif flow.request.path == '/pk_json/' and flow.request.method == 'GET':
-        flow.response = http.Response.make(
-            200,
-            json.dumps({
-                'N': 42, # TODO: replace 42 with the modulus N
-                'e': 47, # TODO: replace 47 with the public exponent e
-            }),
-            {'content-type': 'application/json'},
-        )
